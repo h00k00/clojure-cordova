2aa165c299533046487899141a102cadbafab6fd
Harri Kaasalainen <harri@taiste.fi>
2017-07-20 11:08:36 +0300

    Add Material-ui

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject template "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
-                 [reagent "0.6.0"]
+                 [cljs-react-material-ui "0.2.46"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom]]
                  [re-frame "0.9.4"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
--- src/cljs/template/views.cljs
+++ src/cljs/template/views.cljs
@@ -1,7 +1,30 @@
 (ns template.views
-  (:require [re-frame.core :as re-frame]))
+  (:require
+    [cljsjs.material-ui]
+    [cljs-react-material-ui.core :refer [get-mui-theme color]]
+    [cljs-react-material-ui.reagent :as ui]
+    [cljs-react-material-ui.icons :as ic]
+    [re-frame.core :as re-frame]
+    [reagent.core :as r]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello from " @name])))
+;      [:div "Hello from " @name])))
+      [ui/mui-theme-provider
+       {:mui-theme (get-mui-theme
+                     {:palette {:text-color (color :green600)}})}
+       [:div
+        [ui/app-bar {:title "Title"
+                      :icon-element-right
+                       (r/as-element [ui/icon-button
+                                        (ic/action-account-balance-wallet)])}]
+        [ui/paper
+         [:div "Hello"]
+         [ui/mui-theme-provider
+          {:mui-theme (get-mui-theme {:palette {:text-color (color :blue200)}})}
+          [ui/raised-button {:label "Blue button"}]]
+         (ic/action-home {:color (color :grey600)})
+         [ui/raised-button {:label        "Click me"
+                             :icon         (ic/social-group)
+                             :on-touch-tap #(println "clicked")}]]]])))
